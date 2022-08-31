fe5019c2ab9d6e0e6b0ee349720b909983580775
Joeri Abbo <joeriabbo@hotmail.com>
2022-08-31 23:47:49 +0200

    Icttrainingen : Persisting Entities Using the Entity Manager.

--- src/main/java/com/skillsoft/jpa/App.java
+++ src/main/java/com/skillsoft/jpa/App.java
@@ -1,13 +1,25 @@
 package com.skillsoft.jpa;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class App {
+    public static void main(String[] args) {
+        EntityManagerFactory factory = Persistence.createEntityManagerFactory("BookstoreDB_Unit");
+        EntityManager entityManager = factory.createEntityManager();
+
+        entityManager.getTransaction().begin();
+
+        Book firstBook = new Book(1234, "The Java Language Specification", "Gilad Bracha", 99);
+        Book secondBook = new Book(2222, "The Java Language Specification Second Edition", "Gilad Bracha, James Gosling", 119);
+
+        entityManager.persist(firstBook);
+        entityManager.persist(secondBook);
+
+        entityManager.getTransaction().commit();
+
+        entityManager.close();
+        factory.close();
     }
 }
