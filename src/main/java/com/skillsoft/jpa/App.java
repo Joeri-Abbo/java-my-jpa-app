d8e3ee6b9bd9a65a84fa8b0ab5a402f5f88f0f84
Joeri Abbo <joeriabbo@hotmail.com>
2022-09-02 00:12:06 +0200

    Icttrainingen : Read Operations.

--- src/main/java/com/skillsoft/jpa/App.java
+++ src/main/java/com/skillsoft/jpa/App.java
@@ -3,29 +3,33 @@ package com.skillsoft.jpa;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-import java.util.Calendar;
-import java.util.GregorianCalendar;
+import java.util.List;
 
 public class App {
+
+    @SuppressWarnings("unchecked")
     public static void main(String[] args) {
         EntityManagerFactory factory = Persistence.createEntityManagerFactory("BookstoreDB_Unit");
         EntityManager entityManager = factory.createEntityManager();
 
         try {
-            entityManager.getTransaction().begin();
 
-            Book firstBook = new Book("The Java Language Specification", "Gilad Bracha", 99.99999f);
-            Book secondBook = new Book("The Java Language Specification Second Edition", "Gilad Bracha, James Gosling", 119f);
-            Book thirdBook = new Book("Core Java Volume I", "Cay S. Horstmann", 59.9999f);
+//            Book bookOne = entityManager.find(Book.class, 221);
+//            System.out.println(bookOne);
+//
+//            Book bookTwo = entityManager.find(Book.class, 251);
+//            System.out.println(bookTwo);
+//
+//            Book bookThree = entityManager.find(Book.class,281);
+//            System.out.println(bookThree);
 
-            entityManager.persist(firstBook);
-            entityManager.persist(secondBook);
-            entityManager.persist(thirdBook);
+            List<Book> books = entityManager.createQuery("SELECT b FROM Book b", Book.class).getResultList();
+
+            System.out.println(books);
 
         } catch (Exception e) {
             System.out.println("An exception occurred: " + e);
         } finally {
-            entityManager.getTransaction().commit();
 
             entityManager.close();
             factory.close();
