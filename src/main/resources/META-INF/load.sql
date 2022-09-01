e0ff8c2da6e28c747c7d7d5e5223785d2e31cda0
Joeri Abbo <joeriabbo@hotmail.com>
2022-09-02 00:13:30 +0200

    Icttrainingen : Read Operations.

--- src/main/resources/META-INF/load.sql
+++ src/main/resources/META-INF/load.sql
@@ -1,2 +1,4 @@
 INSERT INTO `bookstoredb`.`Book` values (221, "Thinking in Java", 29.99, "Bruce Eckel");
-INSERT INTO `bookstoredb`.`Book` values (251, "Effective Java 2nd Editor", 19.99, "Joshua Bloch");
+INSERT INTO `bookstoredb`.`Book` values (231, "Effective Java 2nd Editor", 19.99, "Joshua Bloch");
+INSERT INTO `bookstoredb`.`Book` values (241, "The Java Language Specifications", 99.99, "Gilad Bracha");
+INSERT INTO `bookstoredb`.`Book` values (251, "Core Java Volume I", 59.99, "Cay S. Horstmann");
