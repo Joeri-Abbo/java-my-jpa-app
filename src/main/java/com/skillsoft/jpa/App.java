9aace9fe109de42fe66a80e8ee7ee375cc90a12b
Joeri Abbo <joeriabbo@hotmail.com>
2022-09-03 23:31:50 +0200

    ICTtrainingen : Executing JPQL Queries.

--- src/main/java/com/skillsoft/jpa/App.java
+++ src/main/java/com/skillsoft/jpa/App.java
@@ -5,23 +5,37 @@ import java.util.List;
 
 public class App {
 
-    @SuppressWarnings("unchecked")
     public static void main(String[] args) {
 
         EntityManagerFactory factory = Persistence.createEntityManagerFactory("OnlineShippingDB_Unit");
         EntityManager entityManager = factory.createEntityManager();
 
         try {
-            Query query = entityManager.createNativeQuery("SELECT * FROM Categories", Category.class);
+//            Query categoryQuery = entityManager.createQuery("SELECT c FROM Categories c");
+//
+//            List<?> categories = categoryQuery.getResultList();
+//
+//            categories.forEach(System.out::println);
+//
+//            Query productQuery = entityManager.createQuery("SELECT p FROM Products p");
+//            List<?> products = productQuery.getResultList();
+//
+//            products.forEach(System.out::println);
+//
+//
+//            System.out.println("Position of first result: " + categoryQuery.getFirstResult());
+//            System.out.println("Max result retrieved: " + categoryQuery.getMaxResults());
+//
+//            @SuppressWarnings("unchecked") List<Category> categoryList = (List<Category>) categoryQuery.getResultList();
+//
+//            System.out.println(categoryList);
 
-            List<Category> categories = (List<Category>) query.getResultList();
+            TypedQuery<Product> productQuery = entityManager.createQuery(
+                    "SELECT p FROM Products p", Product.class
+            );
 
-            categories.forEach(System.out::println);
-
-            query = entityManager.createNativeQuery("SELECT * FROM Products",Product.class);
-            List<Product> products = (List<Product>) query.getResultList();
-
-            products.forEach(System.out::println);
+            List<Product> productList = productQuery.getResultList();
+            System.out.println(productList);
 
         } catch (Exception e) {
             e.printStackTrace();
