80dbc78212f4192413c79933caf0300f4fe2a3b1
Joeri Abbo <joeriabbo@hotmail.com>
2022-09-03 23:40:35 +0200

    ICTtrainingen : Using Named Parameters and Positional Parameters.

--- src/main/java/com/skillsoft/jpa/App.java
+++ src/main/java/com/skillsoft/jpa/App.java
@@ -11,8 +11,8 @@ public class App {
         EntityManager entityManager = factory.createEntityManager();
 
         try {
-            TypedQuery<Product> productQuery = entityManager.createQuery("SELECT p FROM Products p WHERE p.id > :pid ORDER BY price", Product.class);
-            productQuery.setParameter("pid", 1005);
+            TypedQuery<Product> productQuery = entityManager.createQuery("SELECT p FROM Products p WHERE p.id < ?1 ORDER BY price DESC", Product.class);
+            productQuery.setParameter(1, 1008);
             List<Product> products = productQuery.getResultList();
             System.out.println(products);
 
