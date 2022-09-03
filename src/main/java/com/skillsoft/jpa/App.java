3a53100b5064cbca8f41cb5e28651146c1b65d4b
Joeri Abbo <joeriabbo@hotmail.com>
2022-09-03 21:06:19 +0200

    ICTtrainingen : Mapped Superclass: Persisting and Loading Entities.

--- src/main/java/com/skillsoft/jpa/App.java
+++ src/main/java/com/skillsoft/jpa/App.java
@@ -10,26 +10,33 @@ public class App {
         EntityManager entityManager = factory.createEntityManager();
 
         try {
-            entityManager.getTransaction().begin();
-            Department sales = entityManager.find(Department.class, 2);
-//            Employee cora = entityManager.find(Employee.class, 3);
-//            Employee alice = entityManager.find(Employee.class, 1);
+//            entityManager.getTransaction().begin();
+//            FullTimeEmployee alice = new FullTimeEmployee("Alice", 50000);
+//            FullTimeEmployee ben = new FullTimeEmployee("Ben", 45000);
 //
-//            entityManager.detach(sales);
+//            ContractEmployee cora = new ContractEmployee("Cora", 50);
+//            ContractEmployee dennis = new ContractEmployee("Dennis", 60);
 //
-//            System.out.println("Is sales attached:"+ entityManager.contains(sales));
-//            System.out.println("Is Cora attached:"+ entityManager.contains(cora));
-//            System.out.println("Is Alice attached:"+ entityManager.contains(alice));
+//            Department engineering = new Department("Engineering");
+//            engineering.addEmployee(alice);
+//            engineering.addEmployee(ben);
+//
+//            Vendor admin = new Vendor("Administrative");
+//            admin.addEmployee(cora);
+//            admin.addEmployee(dennis);
+//
+//            entityManager.persist(engineering);
+//            entityManager.persist(admin);
 
-            entityManager.remove(sales);
+            Employee alice = entityManager.find(FullTimeEmployee.class, 1);
 
+            System.out.println(alice.getName());
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
-            entityManager.getTransaction().commit();
+//            entityManager.getTransaction().commit();
             entityManager.close();
             factory.close();
-
         }
     }
 }
