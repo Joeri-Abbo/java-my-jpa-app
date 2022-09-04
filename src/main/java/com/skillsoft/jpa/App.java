d7a024636e7544e5506dc2e16c6475c532b21369
Joeri Abbo <joeriabbo@hotmail.com>
2022-09-04 09:06:57 +0200

    ICTtrainingen : Performing Joins.

--- src/main/java/com/skillsoft/jpa/App.java
+++ src/main/java/com/skillsoft/jpa/App.java
@@ -11,9 +11,10 @@ public class App {
         EntityManager entityManager = factory.createEntityManager();
 
         try {
-            TypedQuery<Category> categoryQuery = entityManager.createQuery("SELECT c FROM Categories c INNER JOIN c.products p WHERE p.price > ?1 ", Category.class);
+            TypedQuery<Category> categoryQuery = entityManager.createQuery("SELECT c FROM Categories c INNER JOIN FETCH c.products p WHERE c.name = ?1 AND p.price > ?2 ", Category.class);
 
-            categoryQuery.setParameter(1, 80f);
+            categoryQuery.setParameter(1, "Fashion");
+            categoryQuery.setParameter(2, 60f);
             List<Category> categories = categoryQuery.getResultList();
             System.out.println(categories);
 
